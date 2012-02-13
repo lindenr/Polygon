6c8eb99a0e9110a39933c8b10ab93a32075f0b22
Linden Ralph <lindenr@fastmail.fm>
2012-02-13 18:41:58 +0000

    changed a lot

--- polygon.c
+++ polygon.c
@@ -3,6 +3,7 @@
 #include <SDL/SDL_gfxPrimitives.h>
 
 SDL_Surface *orig_image;
+float cur_best = 10000;
 
 SDL_Surface *surface_from(SDL_Surface *sf, int w, int h)
 {
@@ -37,7 +38,10 @@ struct Image *random_change(struct Image *i)
     int n;
     if (RR (10) && i->num_polys > 0)
     {
-        int p = RR(i->num_polys);
+        int p = i->num_polys;
+        /* p will be > 0 if we get to here */
+        /* This makes the chosen poly most likely to be the most recently-made one. */
+        while (--p) if (!RR(2)) break;
         Uint16 *P = &(i->polys[p]->x1);
         Sint16 p16;
         Uint8  *P8 = &(i->polys[p]->r);
@@ -50,8 +54,10 @@ struct Image *random_change(struct Image *i)
             }
 
         for (n = 0; n < 3; ++ n)
+        {
             if (!RR(3))
                 P8[n] += RR(30) - 15;
+        }
 
         if (P8[n] < 40) P8[n] += RR(10);
         else if (P8[n] > 50) P8[n] -= RR(10);
@@ -132,10 +138,20 @@ void mkimg (struct Image *image)
                           polys[i]->r, polys[i]->g, polys[i]->b, polys[i]->a);
 }
 
+#define ABSOLUTIFY(a,b) \
+    if (a > b)  \
+    image->fitness += a - b;\
+    else \
+    image->fitness += b - a
+
+#define JOIN(a,b) a ## b
+#define COL_AT_(u,c) (Uint8)((u&fmt->JOIN(c,mask))>>fmt->JOIN(c,shift))<<fmt->JOIN(c,loss)
 void fitness (struct Image *image)
 {
-    int i, end_data = orig_image->h * orig_image->pitch;
-    char *timg, *oimg;
+    int i, end_data = orig_image->h * orig_image->pitch / 4;
+    Uint32 *timg, *oimg, da, db;
+    Uint8 c_r, c_g, c_b, o_r, o_g, o_b;
+    SDL_PixelFormat *fmt = orig_image->format;
     //if (image->fitness != ~0) return;
     image->fitness = 0;
     mkimg(image);
@@ -145,12 +161,19 @@ void fitness (struct Image *image)
     SDL_LockSurface(orig_image);
     oimg = orig_image->pixels;
     for (i = 0; i < end_data; ++ i)
-        /* Seems to be faster than fabs() */
-        if (timg[i] > oimg[i])
-            image->fitness += timg[i] - oimg[i];
-        else
-            image->fitness += oimg[i] - timg[i];
-    printf("Fitness: %d\n", image->fitness);
+    {
+        da = timg[i];
+        db = oimg[i];
+        c_r = COL_AT_(da, R);
+        c_g = COL_AT_(da, G);
+        c_b = COL_AT_(da, B);
+        o_r = COL_AT_(db, R);
+        o_g = COL_AT_(db, G);
+        o_b = COL_AT_(db, B);
+        ABSOLUTIFY(c_r, o_r);
+        ABSOLUTIFY(c_g, o_g);
+        ABSOLUTIFY(c_b, o_b);
+    }
     SDL_UnlockSurface(image->img);
     SDL_UnlockSurface(orig_image);
 }
@@ -159,7 +182,6 @@ struct Image *thebest(struct Image **images, int num_images)
 {
     int i;
     struct Image *ret = images[0];
-    printf("FITNESS\n");
     for (i = 0; i < num_images; ++ i)
         fitness(images[i]);
     for (i = 1; i < num_images; ++ i)
@@ -173,12 +195,46 @@ struct Image *thebest(struct Image **images, int num_images)
     }
     return ret;
 }
+
 // Relies 32-bit and width=256px TODO
 #define PIXEL_AT(x,y) ((y<<10) + (x<<2))
+void new_polygon()
+{
+    struct Polygon *pl;
+    int s = RR(10)+12;
+    int x = RR(256-2*s)+s, y = RR(256-2*s)+s;
+    Uint32 u;
+    SDL_PixelFormat *fmt;
+    pl = malloc(sizeof(struct Polygon));
+    pl->x1 = x-s;
+    pl->y1 = y-s;
+    pl->x2 = x+s;
+    pl->y2 = y-s;
+    pl->x3 = x+s+s;
+    pl->y3 = y;
+    pl->x4 = x+s;
+    pl->y4 = y+s;
+    pl->x5 = x-s;
+    pl->y5 = y+s;
+    pl->x6 = x-s-s;
+    pl->y6 = y;
+    SDL_LockSurface (orig_image);
+    fmt = orig_image->format;
+    int wh = PIXEL_AT(x, y);
+    //printf("Location: %d\n", wh);
+    //SDL_Delay(1000);
+    u = *(Uint32*)orig_image->pixels+wh;
+    SDL_UnlockSurface (orig_image);
+    pl->r = COL_AT_(u,R);
+    pl->g = COL_AT_(u,G);
+    pl->b = COL_AT_(u,B);
+    pl->a = 35;
+    add_poly(cur_image, pl);
+}
+
 int main_loop()
 {
     SDL_Event event;
-    struct Polygon *pl;
     while (SDL_PollEvent(&event))
     {
         switch (event.type)
@@ -189,34 +245,7 @@ int main_loop()
             }
             case SDL_KEYDOWN:
             {
-                int x = RR(166)+45, y = RR(166)+45, s = RR(10)+12;
-                Uint32 u;
-                SDL_PixelFormat *fmt;
-                pl = malloc(sizeof(struct Polygon));
-                pl->x1 = x-s;
-                pl->y1 = y-s;
-                pl->x2 = x+s;
-                pl->y2 = y-s;
-                pl->x3 = x+s+s;
-                pl->y3 = y;
-                pl->x4 = x+s;
-                pl->y4 = y+s;
-                pl->x5 = x-s;
-                pl->y5 = y+s;
-                pl->x6 = x-s-s;
-                pl->y6 = y;
-                SDL_LockSurface (orig_image);
-                fmt = orig_image->format;
-                int wh = PIXEL_AT(x, y);
-                //printf("Location: %d\n", wh);
-                //SDL_Delay(1000);
-                u = *(Uint32*)orig_image->pixels+wh;
-                SDL_UnlockSurface (orig_image);
-                pl->r = ((u&fmt->Rmask)>>fmt->Rshift)<<fmt->Rloss;
-                pl->g = ((u&fmt->Gmask)>>fmt->Gshift)<<fmt->Gloss;
-                pl->b = ((u&fmt->Bmask)>>fmt->Bshift)<<fmt->Bloss;
-                pl->a = 35;
-                add_poly(cur_image, pl);
+                new_polygon();
                 break;
             }
         }
@@ -225,6 +254,10 @@ int main_loop()
                             random_change(duplicate_image(cur_image)),
                             random_change(duplicate_image(cur_image))};
     cur_image = thebest(ptrs, 3);
+    float cur = ((float)cur_image->fitness)/65536;
+    if (cur < cur_best)
+        printf("Current best: %f\n", cur);
+    cur_best = cur;
     return 0;
 }
 
@@ -264,7 +297,7 @@ int main (int argc, char *argv[])
     memset(cur_image, 0, sizeof(*cur_image));
     cur_image->fitness = ~0;
 
-    orig_image = crop_surface(orig_image, 0, 0, 256, 256);
+    orig_image = crop_surface(orig_image, 128, 128, 256, 256);
     cur_image->img = surface_from(orig_image, 256, 256);
     SDL_BlitSurface(orig_image, NULL, screen, &dest);
     SDL_UpdateRects(screen, 1, &dest);
